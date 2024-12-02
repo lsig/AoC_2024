b55469ef59fd64d58ec2d5c7c005bc0e5713b5bd
Lsig <logis21@ru.is>
2024-12-02 00:38:27 -0500

    fix: gitignore

--- day_1/target/debug/deps/day_1-83e0f29c1333af35.d
+++
@@ -1,5 +0,0 @@
-/Users/logisigurdarson/repos/advent_of_code2024/day_1/target/debug/deps/libday_1-83e0f29c1333af35.rmeta: src/main.rs
-
-/Users/logisigurdarson/repos/advent_of_code2024/day_1/target/debug/deps/day_1-83e0f29c1333af35.d: src/main.rs
-
-src/main.rs:
