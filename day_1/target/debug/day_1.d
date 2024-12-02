71668911f99b8f125572f3db78646f7c33cc8082
Lsig <logis21@ru.is>
2024-12-02 00:39:23 -0500

    Remove target

--- day_1/target/debug/day_1.d
+++
@@ -1,1 +0,0 @@
-/Users/logisigurdarson/repos/advent_of_code2024/day_1/target/debug/day_1: /Users/logisigurdarson/repos/advent_of_code2024/day_1/src/main.rs
--- day_1/target/debug/deps/day_1-35f54e7a1179f972.d
+++
@@ -1,5 +0,0 @@
-/Users/logisigurdarson/repos/advent_of_code2024/day_1/target/debug/deps/day_1-35f54e7a1179f972: src/main.rs
-
-/Users/logisigurdarson/repos/advent_of_code2024/day_1/target/debug/deps/day_1-35f54e7a1179f972.d: src/main.rs
-
-src/main.rs:
