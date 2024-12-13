b5388151e6260c58fdd074e21d22be603ee37703
Lsig <logis21@ru.is>
2024-12-13 18:23:00 -0500

    feat: Day 2 complete

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -4,6 +4,31 @@ use std::{
     io::{self, BufRead, BufReader},
 };
 
+fn make_combinations(slice: &[i64]) -> Vec<Vec<i64>> {
+    let mut combinations: Vec<Vec<i64>> = (0..slice.len())
+        .map(|i| {
+            slice
+                .iter()
+                .take(i)
+                .chain(slice.iter().skip(i + 1))
+                .copied()
+                .collect()
+        })
+        .collect();
+
+    combinations.push(slice.to_vec());
+    combinations
+}
+
+fn safety_check(slice: &[i64]) -> bool {
+    slice
+        .windows(2)
+        .all(|w| w[0] < w[1] && i64::abs(w[1] - w[0]) >= 1 && i64::abs(w[1] - w[0]) <= 3)
+        || slice
+            .windows(2)
+            .all(|w| w[0] > w[1] && i64::abs(w[1] - w[0]) >= 1 && i64::abs(w[1] - w[0]) <= 3)
+}
+
 fn main() -> io::Result<()> {
     let fd = File::open("input.txt")?;
     let reader = BufReader::new(fd);
@@ -20,18 +45,36 @@ fn main() -> io::Result<()> {
 
     let safe = reports
         .iter()
-        .map(|report| {
-            report
-                .windows(2)
-                .all(|w| w[0] < w[1] && i64::abs(w[1] - w[0]) >= 1 && i64::abs(w[1] - w[0]) <= 3)
-                || report.windows(2).all(|w| {
-                    w[0] > w[1] && i64::abs(w[1] - w[0]) >= 1 && i64::abs(w[1] - w[0]) <= 3
-                })
-        })
+        .map(|report| safety_check(&report))
         .filter(|&safe| safe)
         .count();
 
+    let safe2 = reports
+        .iter()
+        .map(|report| make_combinations(&report))
+        .filter(|report| report.iter().any(|combination| safety_check(&combination)))
+        .count();
+
     println!("{:?}", safe);
+    println!("{:?}", safe2);
 
     Ok(())
+}
+
+#[test]
+fn test_make_combinations() {
+    let input = vec![1, 2, 3, 4];
+    let mut result = make_combinations(&input);
+    let mut test = vec![
+        vec![1, 2, 3],
+        vec![1, 3, 4],
+        vec![2, 3, 4],
+        vec![1, 2, 4],
+        input,
+    ];
+
+    result.sort();
+    test.sort();
+
+    assert_eq!(result, test);
 }
