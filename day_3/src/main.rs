85025ed8aee50ff2adad88e7ad151fa0cbf0bf39
Lsig <logis21@ru.is>
2024-12-14 16:56:32 -0500

    feat: Day 3 part 1

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -9,17 +9,23 @@ fn main() -> io::Result<()> {
     let fd = File::open("input.txt")?;
     let reader = BufReader::new(fd);
 
-    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
-    let mul: Vec<String> = reader
+    let mul: i64 = reader
         .lines()
         .filter_map(|s| s.ok())
-        .flat_map(|s| {
-            re.find_iter(&s)
-                .map(|m| m.as_str().to_string())
-                .collect::<Vec<String>>()
+        .map(|s| {
+            re.captures_iter(&s)
+                .filter_map(|c| {
+                    let (_, [s1, s2]) = c.extract();
+                    let n1 = s1.parse::<i64>().ok()?;
+                    let n2 = s2.parse::<i64>().ok()?;
+
+                    Some(n1 * n2)
+                })
+                .sum::<i64>()
         })
-        .collect();
+        .sum();
 
     println!("{:?}", mul);
 
