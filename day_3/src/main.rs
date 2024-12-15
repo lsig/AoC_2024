0b7859953dd8220692767f59e2cc444c17150d7f
Lsig <logis21@ru.is>
2024-12-14 19:17:23 -0500

    feat: Day 3 complete

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -5,6 +5,13 @@ use std::{
 
 use regex::Regex;
 
+#[derive(Debug)]
+enum Command {
+    Mult(i64, i64),
+    Do,
+    Dont,
+}
+
 fn main() -> io::Result<()> {
     let fd = File::open("input.txt")?;
     let reader = BufReader::new(fd);
@@ -28,6 +35,43 @@ fn main() -> io::Result<()> {
         .sum();
 
     println!("{:?}", mul);
+
+    let fd2 = File::open("input.txt")?;
+    let reader2 = BufReader::new(fd2);
+
+    let re2 = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))").unwrap();
+    let mut do_mul = 1;
+
+    let mul2: i64 = reader2
+        .lines()
+        .filter_map(|s| s.ok())
+        .flat_map(|s| {
+            re2.captures_iter(&s)
+                .filter_map(|c| match (c.get(1), c.get(2), c.get(3), c.get(4)) {
+                    (Some(s1), Some(s2), None, None) => Some(Command::Mult(
+                        s1.as_str().parse::<i64>().ok()?,
+                        s2.as_str().parse::<i64>().ok()?,
+                    )),
+                    (None, None, Some(_), None) => Some(Command::Do),
+                    (None, None, None, Some(_)) => Some(Command::Dont),
+                    _ => None,
+                })
+                .filter_map(|cmd| match cmd {
+                    Command::Mult(n1, n2) => Some(n1 * n2 * do_mul),
+                    Command::Do => {
+                        do_mul = 1;
+                        None
+                    }
+                    Command::Dont => {
+                        do_mul = 0;
+                        None
+                    }
+                })
+                .collect::<Vec<i64>>()
+        })
+        .sum();
+
+    println!("{:?}", mul2);
 
     Ok(())
 }
